3f876ae1452303ec720cf594f4dec85d75029717
linamaria <linamariaortegaimbus123@gmail.com>
2025-03-12 12:36:31 -0500

    v1

--- src/componentes/administrador/PanelInvernadero.jsx
+++ src/componentes/administrador/PanelInvernadero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Invernaderoi from '../../imagenes/Invernadero.jpg';
 
 function PanelInvernadero() {
   return ( 
@@ -10,19 +11,22 @@ function PanelInvernadero() {
     <main>
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="max-w-sm w-full bg-white rounded-lg shadow-lg">
-        <img src="..." alt="..." className="w-full h-48 object-cover rounded-t-lg" />
+        <img src={Invernaderoi} alt="magen" className="w-full h-25 object-cover rounded-t-lg" />
         <div className="p-6 text-center">
           <a
             href="#"
             className="inline-block bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-700"
           >
             Invernadero 1
+            
           </a>
         </div>
       </div>
     </div>
-
-
+    
+    <button className="bg-green-500 text-white py-2 px-5 rounded-md w-1/2 mx-auto hover:bg-green-600">
+            Agregar Invernadero
+    </button>
     </main>
     <footer>
 
---
+++ src/vistas/Invernaderos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PanelInvernadero from '../componentes/administrador/PanelInvernadero'
+
+export default function Invernaderos() {
+  return (
+    <>
+        <PanelInvernadero />
+    </>
+    
+  )
+}
