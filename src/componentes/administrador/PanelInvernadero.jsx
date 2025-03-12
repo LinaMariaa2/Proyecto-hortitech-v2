ff3f6d7a6e419e0736f28d6ee7c54f8eaf177bd6
linamaria <linamariaortegaimbus123@gmail.com>
2025-03-12 12:00:41 -0500

    panel

---
+++ src/componentes/administrador/PanelInvernadero.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function PanelInvernadero() {
+  return ( 
+  <>
+    <header>
+
+
+    </header>
+    <main>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="max-w-sm w-full bg-white rounded-lg shadow-lg">
+        <img src="..." alt="..." className="w-full h-48 object-cover rounded-t-lg" />
+        <div className="p-6 text-center">
+          <a
+            href="#"
+            className="inline-block bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-700"
+          >
+            Invernadero 1
+          </a>
+        </div>
+      </div>
+    </div>
+
+
+    </main>
+    <footer>
+
+        
+    </footer>
+  
+  
+  
+  </>
+    
+  );
+}
+
+export default PanelInvernadero;
+
+
